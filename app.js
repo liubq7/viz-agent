b96b5d1a8c9eae7c7b7da5e93edc929b8d4d4b0c
liubq7 <liubq97@gmail.com>
2021-07-16 09:34:49 +0800

    update

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 import WebSocket from 'ws';
 
 const ws = new WebSocket("ws://localhost:18115");
+let data = {txs: []};
 
-ws.onopen = function (evt) {
+ws.on('open', function () {
   console.log("CONNECTED");
   ws.send(
     '{"id": 2, "jsonrpc": "2.0", "method": "subscribe", "params": ["new_transaction"]}'
   );
-};
-ws.onmessage = function (evt) {
-  console.log(evt.data);
-};
-ws.onerror = function (evt) {
-  console.log("ERROR:" + evt);
-};
-ws.onclose = function (evt) {
+});
+ws.on('message', function (e) {
+  // console.log(e);
+  data.txs.push(e);
+});
+ws.on('error', function (e) {
+  console.log("ERROR:" + e);
+});
+ws.on('close', function () {
   console.log("DISCONNECTED");
-};
+});
+
+const sendData = async() => {
+  try {
+    await fetch(`${server_path}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: data
+    });
+    data.txs = [];
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+setInterval(async function() {
+  await sendData();
+}, 60000);
