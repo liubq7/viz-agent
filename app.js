f7d6ff89c7ee42cf52c8afde23a2098318617c90
liubq7 <liubq97@gmail.com>
2021-07-25 16:51:23 +0800

    update data format

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import WebSocket from 'ws';
+import axios from "axios";
 
+const node_id = 1; // id of this node stored in psql
 const ws = new WebSocket("ws://localhost:18115");
 let data = {txs: []};
 
@@ -10,8 +12,13 @@ ws.on('open', function () {
   );
 });
 ws.on('message', function (e) {
-  // console.log(e);
-  data.txs.push(e);
+  const res = JSON.parse(e);
+  if (res.result) return;
+
+  const tx = JSON.parse(res.params.result).transaction;
+  const tx_hash = tx.hash;
+  const unix_timestamp = Date.now();
+  data.txs.push({node_id, tx_hash, unix_timestamp});
 });
 ws.on('error', function (e) {
   console.log("ERROR:" + e);
@@ -22,12 +29,7 @@ ws.on('close', function () {
 
 const sendData = async() => {
   try {
-    await fetch(`${server_path}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: data
-    });
-    data.txs = [];
+    await axios.post("http://localhost:3006/txs", data).then(data.txs = []);
   } catch (err) {
     console.error(err.message);
   }
